7528bc929cf3e9ef1d4f580bbcda516936c8c608
Andrey <andreybespaliy69@gmail.com>
2024-07-10 22:58:48 +0300

    feat: Added validation to product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,12 +8,15 @@ import {
     deleteProduct,
 } from '../controllers/productController.js';
 
+import { handleErrors } from '../utils/handleErrors.js';
+import { productValidation } from '../validations/productValidation.js';
+
 const router = express.Router();
 
-router.post('/products', createProduct);
+router.post('/products', productValidation, handleErrors, createProduct);
 router.get('/products', getProducts);
 router.get('/products/:id', getProduct);
-router.patch('/products/:id', updateProduct);
+router.patch('/products/:id', productValidation, handleErrors, updateProduct);
 router.delete('products/:id', deleteProduct);
 
 export default router;
